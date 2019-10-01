8c91e26166fa15bd598085119a07fea01037b9be
rohan01939 <rohan01939@gmail.com>
2019-10-01 00:28:12 -0400

    day1

---
+++ src/dfg/org/or/Cvc.java
@@ -0,0 +1,30 @@
+package dfg.org.or;
+
+public class Cvc {
+	public void rohan() {
+		String a="Aktarul is ";
+		String b="Rohan is ";
+		int c=14;
+		int d=10;
+		int f=c+d;
+		String e=" years old.";
+		System.out.println(a+c+e);
+		System.out.println(b+f+e);
+		
+	}
+	public void hridoy() {
+		String a="He lives in";
+		String b=" The bronx";
+		String c=" Since ";
+		int d=2011;
+		System.out.println(a+b+c+d);
+		
+	}
+public void friends(String a, int b) {
+	String c=" Live in the Bronx since ";
+	System.out.println(a+c+b);
+	
+}
+	
+	
+}
---
+++ src/dfg/org/or/Tester.java
@@ -0,0 +1,14 @@
+package dfg.org.or;
+
+public class Tester {
+public static void main(String[] args) {
+	Cvc cvc=new Cvc();
+	cvc.rohan();
+	
+	cvc.hridoy();
+	cvc.friends("Piyash",2012);
+	cvc.friends("Rohan", 2013);
+	cvc.friends("Hridoy", 2014);
+}
+
+}
